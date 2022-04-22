3440e54e474d91b585678e332eb5b20b24763f4d
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-04-21 22:48:49 -0300

    Class Departamentos e Cargos criadas e Mapeamentos

---
+++ src/main/java/com/ads/dev/boot/domain/Cargo.java
@@ -0,0 +1,38 @@
+package com.ads.dev.boot.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "CARGOS")
+public abstract class Cargo extends AbstractEntity<Long> {
+	
+	@Column(name = "nome", nullable = false, unique = true, length = 60)
+	private String nome;
+	
+	@ManyToOne // Um Departamento pode ter muitos cargos
+	@JoinColumn(name = "id_departamento_fk") // chave estrangeira no banco
+	private Departamento departamento;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+	
+
+}
