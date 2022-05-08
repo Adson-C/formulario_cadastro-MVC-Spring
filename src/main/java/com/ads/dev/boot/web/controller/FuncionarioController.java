442a2469086e7136888bcafb944e82af70097bb2
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-05-08 12:10:31 -0300

    Inserindo Funcionarios

--- src/main/java/com/ads/dev/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/ads/dev/boot/web/controller/FuncionarioController.java
@@ -1,15 +1,33 @@
 package com.ads.dev.boot.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.ads.dev.boot.domain.Cargo;
+import com.ads.dev.boot.domain.Funcionario;
+import com.ads.dev.boot.domain.UF;
+import com.ads.dev.boot.service.CargoService;
+import com.ads.dev.boot.service.FuncionarioService;
 
 @Controller
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 	
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
+	@Autowired
+	private CargoService cargoService;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Funcionario funcionario) {
 		return "funcionario/cadastro";
 	}
 
@@ -18,4 +36,24 @@ public class FuncionarioController {
 		return "funcionario/lista";
 	}
 
+	@PostMapping("/salvar")
+	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+		
+		funcionarioService.salvar(funcionario);
+		attr.addFlashAttribute("success", "Funcionario inserido com sucesso.");
+		
+		return "redirect:/funcionarios/cadastrar"; // depois que salvar redireciona para pagina de cadastro
+		
+	}
+	
+	@ModelAttribute("cargos")
+	public List<Cargo> getCargos() {
+		return cargoService.buscarTodos();
+	}
+	
+	@ModelAttribute("ufs")
+	public UF[] getUFs() {
+		return UF.values();
+	}
+	
 }
