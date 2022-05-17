f0c7335cea3adf3404d5a09c2c294ad83f9e8cdd
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-05-17 20:45:07 -0300

    Validando Fucionario e endereço, Valiadando campo a campo para funcionario

--- src/main/java/com/ads/dev/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/ads/dev/boot/web/controller/FuncionarioController.java
@@ -3,9 +3,12 @@ package com.ads.dev.boot.web.controller;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,7 +46,11 @@ public class FuncionarioController {
 	}
 
 	@PostMapping("/salvar")
-	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+	public String salvar(@Valid Funcionario funcionario,BindingResult result ,RedirectAttributes attr) {
+		
+		if (result.hasErrors()) {
+			return "/funcionario/cadastro";
+		}
 		
 		funcionarioService.salvar(funcionario);
 		attr.addFlashAttribute("success", "Funcionario inserido com sucesso.");
@@ -59,7 +66,12 @@ public class FuncionarioController {
 	}
 	
 	@PostMapping("/editar")
-	public String editar(Funcionario funcionario, RedirectAttributes attr) {
+	public String editar(@Valid Funcionario funcionario,BindingResult result ,RedirectAttributes attr) {
+		
+		if (result.hasErrors()) {
+			return "/funcionario/cadastro";
+		}
+		
 		funcionarioService.editar(funcionario);
 		attr.addFlashAttribute("success", "Funcionario editado com sucesso.");
 		
