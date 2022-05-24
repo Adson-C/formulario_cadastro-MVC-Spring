e4de51df6695edd0661ced7216ec04e2c5581c95
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-05-23 23:28:02 -0300

    Preparando o controller e açoes de componentes

--- src/main/java/com/ads/dev/boot/web/controller/CargoController.java
+++ src/main/java/com/ads/dev/boot/web/controller/CargoController.java
@@ -1,6 +1,7 @@
 package com.ads.dev.boot.web.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -13,12 +14,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ads.dev.boot.domain.Cargo;
 import com.ads.dev.boot.domain.Departamento;
 import com.ads.dev.boot.service.CargoService;
 import com.ads.dev.boot.service.DepartamenntoService;
+import com.ads.dev.boot.util.PaginacaoUtil;
 
 @Controller
 @RequestMapping("/cargos")
@@ -36,9 +39,14 @@ public class CargoController {
 	}
 
 	@GetMapping("/listar")
-	public String listar(ModelMap model) {
-		model.addAttribute("cargos", cargoService.buscarTodos());
-		return "/cargo/lista";
+	public String listar(ModelMap model, @RequestParam("page") Optional<Integer> page) {
+		
+		int paginaAtual = page.orElse(1);
+		
+		PaginacaoUtil<Cargo> pageCargo = cargoService.buscarPorPagina(paginaAtual);
+		
+		model.addAttribute("pageCargo", pageCargo);
+		return "cargo/lista";
 	}
 	
 	@PostMapping("/salvar")
